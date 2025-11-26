e242e4194b25ac143ec142176f3fced17b356dbd
Fozz-svg <kowfoz1@hotmail.co.uk>
2025-11-26 20:17:05 +0000

    Update frontend to use correct backend URL

--- frontend/Program.cs
+++ frontend/Program.cs
@@ -8,7 +8,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 var apiUrl = builder.HostEnvironment.IsDevelopment()
     ? "http://localhost:5160"
-    : "https://helloworldapi.azurewebsites.net";
+    : "https://helloworldapi-b4gkg6ccdaegbaas.uksouth-01.azurewebsites.net";
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiUrl) });
 
